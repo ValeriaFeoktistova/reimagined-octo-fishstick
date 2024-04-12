e668536f864875b4201ad79682f564be896336a3
V-feo <v-feoktistova@inbox.ru>
2024-04-12 15:01:18 +0700

    1

--- src/g.java
+++ src/g.java
@@ -1,5 +1,5 @@
 public class g {
     public static void main(String[] args) {
-        System.out.println("Hello world!"+"1"+"2777777888888888888888888888888");
+        System.out.println("Hello world!"+"1"+"2777777888888888888888888888888"+"333333333333333333");
     }
 }
